dc33fee8cc1f285f6892759ea3955caaa004de2a
Isaac-rosdail <idrfritcher@gmail.com>
2025-03-20 21:36:25 +0000

    Add folders for Flask Web UI, basic Flask webpage Update PROJECT_LOG.md

--- core/app.py
+++ core/app.py
@@ -1,1 +1,18 @@
 # Main Flask app -- import grocery module
+
+from flask import Flask, redirect, url_for, render_template
+import datetime
+
+# Display current time on splash screen
+current_time = datetime.datetime.now()
+time_display = current_time.strftime("%H:%M:%S")
+date_display = current_time.strftime("%A, %B %d")
+
+app = Flask(__name__, template_folder="../templates")
+
+@app.route("/")
+def home():
+    return render_template("index.html", time_display=time_display, date_display=date_display)
+
+if __name__ == "__main__":
+    app.run()
