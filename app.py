64a5e2a177e2219ae571098b7e464ede6f7133c1
Isaac-rosdail <idrfritcher@gmail.com>
2025-03-22 21:17:38 +0000

    Reorganize structure a bit, implement functional navbar on homepage template

--- app.py
+++ app.py
@@ -8,11 +8,15 @@ current_time = datetime.datetime.now()
 time_display = current_time.strftime("%H:%M:%S")
 date_display = current_time.strftime("%A, %B %d")
 
-app = Flask(__name__, template_folder="../templates")
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html", time_display=time_display, date_display=date_display)
+
+@app.route("/grocery")
+def grocery():
+    return render_template("barcode_scanner/grocery.html")
 
 @app.route("/add_product")
 def add_product():
