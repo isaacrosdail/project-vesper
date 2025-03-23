6ab15a988a4ce70db5bd4bda7e85cfe535f2b249
Isaac-rosdail <idrfritcher@gmail.com>
2025-03-23 00:38:30 +0000

    Link database to populate table in groceries.html template to display entries

--- modules/groceries/models.py
+++ modules/groceries/models.py
@@ -5,16 +5,26 @@ from sqlalchemy import text
 # Setup function for groceries portion of database
 ## Run this once upon boot up
 def setup_schema():
+	print("[groceries] Running schema setup...")
 	conn = engine.connect()  # Handshake - encapsulates DBAPI, connection string, pooling config?
 	conn.execute(text(
 		"CREATE TABLE IF NOT EXISTS groceries (name str, price float, grams int)"
 		)) # executes statement
 	conn.commit() # applies change(s) to database
+	print("[groceries] Table creation committed.")
 
-# Starts a session with the respective database
-session = get_session()
-session.execute(
-	text('INSERT INTO groceries (name, price, grams) VALUES (:name, :price, :grams)'),
-	{"name": "Strawberry Jam", "price": 5.49, "grams": 250}
-)
-session.commit()
+def get_all_products():
+	session = get_session()
+	result = session.execute(text("SELECT * FROM groceries"))
+	session.close()
+	return result
+
+def add_product(name, price, grams):
+	# Starts a session with the respective database
+	session = get_session()
+	session.execute(
+		text('INSERT INTO groceries (name, price, grams) VALUES (:name, :price, :grams)'),
+		{"name": name, "price": price, "grams": grams}
+	)
+	session.commit()
+	session.close()
