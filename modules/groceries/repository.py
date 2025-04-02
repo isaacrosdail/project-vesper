b2c232cb41804bed071c1e41c9744010adf2b21e
Isaac-rosdail <idrfritcher@gmail.com>
2025-04-02 18:16:47 +0100

    Create separate repository.py for grocery db functions, keep models.py for only db models

---
+++ modules/groceries/repository.py
@@ -0,0 +1,70 @@
+## DB logic functions to access data
+
+from .models import Product, Transaction
+from sqlalchemy.orm import Session, joinedload
+from datetime import date
+from decimal import Decimal
+
+# Lookup barcode function to centralize a bit
+def lookup_barcode(session, barcode):
+	return session.query(Product).filter_by(barcode=barcode).first()
+
+def get_all_products(session):
+	return session.query(Product).all()
+
+# Eager load 'product' relationship using joinedload so we can safely access transaction.product.* fields in templates
+# after session is closed (avoids DetachedInstanceError)
+def get_all_transactions(session):
+	return session.query(Transaction).options(joinedload(Transaction.product)).all()
+
+#####################################
+# Uses **kwargs to take in optional data (ie., from forms)
+# Now used for scanner barcode input only
+def process_scanned_barcode(session, barcode, **product_data):
+	product = lookup_barcode(session, barcode)
+
+	if product:
+		add_transaction(session, product, quantity=1, price_at_scan=product.price, net_weight=product.net_weight)
+		return "added_transaction"
+
+	return "new_product"
+####################################
+
+def ensure_product_exists(session, **product_data):
+	barcode = product_data["barcode"]
+	product = lookup_barcode(session, barcode)
+	if not product:
+		add_product(session, **product_data)
+
+# Add not-previously-encountered product
+def add_product(session, **product_data):
+	product = Product(
+		barcode=product_data["barcode"],
+		product_name=product_data["product_name"], 
+		price=Decimal(product_data["price"]),
+		net_weight=float(product_data["net_weight"])
+	)
+
+	session.add(product)
+
+# Add product to 'inventory'
+def add_transaction(session, product, **product_data):
+	today = date.today()
+	quantity = int(product_data.get("quantity") or 1)
+	
+	# Check to determine whether to increment qty or add new instance
+	transaction = session.query(Transaction).filter_by(
+		product_id=product.product_id,
+		date_scanned=today
+	).first()
+	
+	if transaction:
+		transaction.quantity += quantity
+	else:
+		transaction = Transaction(
+			product_id=product.product_id,
+			price_at_scan=Decimal(product_data["price"]),
+			quantity=quantity,
+			date_scanned=today
+		)
+		session.add(transaction)
