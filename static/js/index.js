0fd7a0d0733563b3260adfb3a0358f9b0a60ccb3
Isaac-rosdail <idrfritcher@gmail.com>
2025-04-02 13:56:11 +0100

    Add JS function to hide button/input field once tether text is added & enable click on text to edit

--- static/js/index.js
+++ static/js/index.js
@@ -2,10 +2,24 @@
 // Variable for tether text input
 let tether;
 
-document.getElementById("tether-submit").onclick = function(){
+// Constant for tether-submit button
+const tetherSubmitBtn = document.getElementById("tether-submit");
+// Variables for tether-text and tether-result
+let tetherText = document.getElementById("tether-text");
+let tetherResult = document.getElementById("tether-result");
+
+tetherSubmitBtn.onclick = () => {
     // Get tether text from input using element id
     tether = document.getElementById("tether-text").value;
     // Now want to display said text instead of input box
     document.getElementById("tether-result").textContent = tether;
+    // And hide the input text box and submit button
+    tetherText.classList.add("hidden");
+    tetherSubmitBtn.classList.add("hidden");
 }
 
+tetherResult.onclick = () => {
+    tetherResult.classList.remove("hidden");
+    tetherText.classList.remove("hidden");
+    tetherSubmitBtn.classList.remove("hidden");
+}
