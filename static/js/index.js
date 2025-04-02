554a7ebc6ed7c79fe75f4aec43e353c73154734e
Isaac-rosdail <idrfritcher@gmail.com>
2025-04-02 12:48:04 +0100

    Add JS function to display text from input for tether-card

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,11 @@
-document.getElementById("tether-submit").onclick = 
+
+// Variable for tether text input
+let tether;
+
+document.getElementById("tether-submit").onclick = function(){
+    // Get tether text from input using element id
+    tether = document.getElementById("tether-text").value;
+    // Now want to display said text instead of input box
+    document.getElementById("tether-result").textContent = tether;
+}
+
