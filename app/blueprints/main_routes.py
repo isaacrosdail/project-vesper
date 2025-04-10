eb0913d050defbdd85c43d0b94b181cf2b15642f
Isaac-rosdail <idrfritcher@gmail.com>
2025-04-10 21:39:24 +0100

    Feature(ui): Anchor habits display on dashboard
    - Begin dashboard display for Anchor Habits
    - Add JS function to mark habits complete via checkbox with immediate POST
    - Small changes, need to stop for now.
    - Update PROJECT_LOG.md

--- app/blueprints/main_routes.py
+++ app/blueprints/main_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
-
+from app.database import get_session
+from app.modules.tasks import repository as tasks_repo
 # Other imports
 from datetime import datetime
 
@@ -7,8 +8,28 @@ main_bp = Blueprint('main', __name__)
 
 @main_bp.route("/")
 def home():
+
     # Display current time on splash screen
     current_time = datetime.now()
     time_display = current_time.strftime("%H:%M:%S")
     date_display = current_time.strftime("%A, %B %d")
-    return render_template("index.html", time_display=time_display, date_display=date_display)
+    
+    # Enable checking whether completed_at == today
+    today = datetime.today()
+    
+    # Get Tasks to pass Anchor Habits to display
+    session = get_session()
+
+    # Get all tasks
+    tasks = tasks_repo.get_all_tasks(session)
+    # Then filter anchor habits from that
+    anchor_habits = [
+        task for task in tasks
+        if task.type == "habit" and task.is_anchor
+    ]
+
+    return render_template("index.html", 
+                           time_display=time_display, 
+                           date_display=date_display,
+                           today=today,
+                           anchor_habits = anchor_habits)
