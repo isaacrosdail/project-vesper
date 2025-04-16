58933558480ec6555a6c1bbd444134750948524f
Isaac-rosdail <idrfritcher@gmail.com>
2025-04-16 15:53:20 +0100

    Transition to Tailwind, re-create navbar styling
    - Update PROJECT_LOG.md

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+      './app/**/*.{html,js}',
+      './static/**/*.{css,js}',
+      './templates/**/*.html'
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
