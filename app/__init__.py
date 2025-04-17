f495880053f978d13b45989bda6d934267f83741
Isaac-rosdail <idrfritcher@gmail.com>
2025-04-17 16:40:22 +0100

    Implement generalized DELETE route in crud_routes.py, create unit test to confirm it works

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from app.modules.tasks import models as tasks_models
 from app.core.routes import main_bp
 from app.modules.groceries.groceries_routes import groceries_bp
 from app.modules.tasks.routes import tasks_bp
+from app.modules.crud_routes import crud_bp
 
 def create_app(config_name="dev"): # Default to DevConfig if nothing is passed in
     app = Flask(__name__)
@@ -26,6 +27,7 @@ def create_app(config_name="dev"): # Default to DevConfig if nothing is passed i
         db_session.remove()
 
     app.register_blueprint(main_bp)
+    app.register_blueprint(crud_bp)
     app.register_blueprint(groceries_bp)
     app.register_blueprint(tasks_bp)
 
--- app/modules/crud_routes.py
+++ app/modules/crud_routes.py
@@ -4,4 +4,40 @@
 from flask import Blueprint, render_template, redirect, url_for, request, jsonify
 from app.core.database import db_session
 
-### Steps to Generalize ADD
+# Import models
+from app.modules.groceries import models as grocery_models
+from app.modules.tasks import models as tasks_models
+
+# right?
+crud_bp = Blueprint("crud", __name__)
+
+### Generalizing to be able to support all CRUD ops through one generalized engine that supports:
+# 1. Any number of modules (eg, groceries, tasks, etc.)
+# 2. Any number of sub-models within each (eg, grocery product, grocery transaction)
+
+# Map to correct model based on module passed in
+# This uses module and subtype to resolve to the specific model of item altered
+modelMap = {
+    ("groceries", "product"): grocery_models.Product,
+    ("groceries", "transaction"): grocery_models.Transaction,
+    ("tasks", "none"): tasks_models.Task,
+}
+
+# DELETE (Product)
+@crud_bp.route("/<module>/<subtype>/<int:item_id>", methods=["DELETE"])
+def delete_item(module, subtype, item_id):
+    session = db_session()
+
+    # Get correct model
+    model = modelMap.get((module, subtype))
+    print(model)
+    item = session.get(model, item_id) # Grab item by id from db
+
+    # If item doesn't exist
+    if not item:
+        return {"error": f"{model.__name__} not found."}, 404
+    
+    db_session.delete(item)
+    db_session.commit()
+    
+    return "", 204     # 204 means No Content (success but nothing to return, used for DELETEs)
