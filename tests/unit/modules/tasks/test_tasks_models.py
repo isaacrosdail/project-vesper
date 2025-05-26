7af8db45f3932f6ec01684fce04a1f24d5e3facb
Isaac-rosdail <idrfritcher@gmail.com>
2025-05-26 15:24:35 +0100

    Change use of sessionmaker to db_session

--- tests/unit/modules/tasks/test_tasks_models.py
+++ tests/unit/modules/tasks/test_tasks_models.py
@@ -1,25 +1,15 @@
-import pytest
-from sqlalchemy import inspect, text
-
-from sqlalchemy.orm import sessionmaker
-from app.core.database import get_engine
-
-# Import Models
+from app.core.database import db_session
 from app.modules.tasks.models import Task
 
 # Imports for exceptions
 from sqlalchemy.exc import IntegrityError
 
 def test_insert_and_query_task(app):
-    engine = get_engine(app.config)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
+    session = db_session()
     task = Task(title="Test Task", type="todo")
     session.add(task)
     session.flush() # instead of commit
 
     result = session.query(Task).filter_by(title="Test Task").first()
     assert result.type == "todo"
-
     session.close()
