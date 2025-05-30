cf7dbf13a4fb8986bad0a1ad1a7b5e3329ce96bc
isaacrosdail <idrfritcher@gmail.com>
2025-05-30 18:02:21 +0100

    Add get_all_habits

--- app/modules/habits/repository.py
+++ app/modules/habits/repository.py
@@ -1,3 +1,7 @@
 # DB logic functions to access data
 
-from .models import Habit
+from .models import Habit, HabitCompletion
+
+# Get all habits
+def get_all_habits(session):
+    return session.query(Habit).all()
