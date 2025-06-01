5af3558cf88f33328a0d0263b6f1c154a00dc07d
isaacrosdail <idrfritcher@gmail.com>
2025-06-01 10:19:32 +0100

    Sort imports

--- alembic/versions/9c378a042e1d_add_habit_habitcompletion_models_remove_.py
+++ alembic/versions/9c378a042e1d_add_habit_habitcompletion_models_remove_.py
@@ -7,9 +7,9 @@ Create Date: 2025-05-30 16:09:39.307466
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '9c378a042e1d'
