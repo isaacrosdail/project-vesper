ae7e0e9d0147bd12d343af752e93c88c06878015
isaacrosdail <idrfritcher@gmail.com>
2025-06-06 13:28:30 +0100

    Add function to get today's daily intention

--- app/modules/habits/repository.py
+++ app/modules/habits/repository.py
@@ -1,7 +1,18 @@
 # DB logic functions to access data
 
-from .models import Habit, HabitCompletion
-
+from .models import Habit, HabitCompletion, DailyIntention
+from sqlalchemy import func
+from datetime import date
 # Get all habits
 def get_all_habits(session):
     return session.query(Habit).all()
+
+# Get today's daily intention
+def get_today_intention(session):
+    today = date.today()
+
+    todayIntention = session.query(DailyIntention).filter(
+        func.date(DailyIntention.created_at) == today
+    ).first()
+    
+    return todayIntention
