8366e48bab64ddf7fc193c1a9a565db339f7e795
isaacrosdail <idrfritcher@gmail.com>
2025-06-08 21:47:27 +0100

    Extract db deletion approach into util function

---
+++ app/utils/db_utils.py
@@ -0,0 +1,30 @@
+
+from sqlalchemy import text
+
+# Delete all data without nuking schema, optionally reset ID sequencing
+def delete_all_db_data(engine, reset_sequences=False):
+    # Delete in dependency order -> children first
+    tables = [
+        "habit_completions", # child: references habits
+        "transaction",       # child: references products
+        "habits",            # parent table
+        "product",           # parent table
+        "tasks"              # independent
+    ]
+    sequences = [
+        "habit_completions_id_seq", # references habits
+        "transaction_id_seq",       # references products
+        "habits_id_seq",            # parent table
+        "product_product_id_seq",   # parent table
+        "tasks_id_seq"
+    ]
+    # New way with Alembic instead of create_all()
+    with engine.begin() as conn:
+        # Delete data
+        for table in tables:
+            conn.execute(text(f"DELETE FROM {table}"))
+
+            # Reset sequences if desired
+            if reset_sequences:
+                for seq in sequences:
+                    conn.execute(text(f"ALTER SEQUENCE {seq} RESTART WITH 1"))
