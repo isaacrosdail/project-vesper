4031478321ac3336babbc354173ccaf404411e32
isaacrosdail <idrfritcher@gmail.com>
2025-06-13 15:43:31 +0100

    feat(ui): hamburger menu
    - Begin hamburger menu implementation
    - Adjust navbar template for hamburger menu implementation

---
+++ app/static/js/navbar.js
@@ -0,0 +1,11 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+    const hamburgerBtn = document.getElementById('hamburger-btn');
+    const mobileNav = document.getElementById('mobile-nav');
+
+    if (hamburgerBtn && mobileNav) {
+        hamburgerBtn.addEventListener('click', () => {
+            mobileNav.classList.toggle('show');
+        })
+    }
+})
