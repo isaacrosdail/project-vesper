0589b91602f442135bd153506fc7ed25f9908fe1
isaacrosdail <idrfritcher@gmail.com>
2025-06-13 18:07:23 +0100

    Feature(ui): JS mobile nav resize logic
    - Add responsive mobile nav cleanup on screen resize (throttled too)
    - Remove console log

--- app/static/js/navbar.js
+++ app/static/js/navbar.js
@@ -12,15 +12,27 @@ window.addEventListener('DOMContentLoaded', () => {
 
 // Resize event listener to hide our mobile-nav (handles our mobile-nav.show state)
 let lastWidth = 640;
+// Throttle our resize check to 200ms
+let currentTime = Date.now(); // returns ms since 1970
+let lastCheckTime = currentTime - 200; // Pretend it's been 200ms since last check
 
 window.addEventListener('resize', function() {
-    // When resize fires, check if window.innerWidth is above or below 640px
-    // console.log(`Current width: ${lastWidth}`);
-    if (window.innerWidth > 640 && lastWidth <= 640) {
-        console.log(`CROSSED!`)
+    currentTime = Date.now();
+    if (currentTime - lastCheckTime >= 200){
+        console.log('Resize triggered!')
+        const mobileNav = document.getElementById('mobile-nav');
+        // When resize fires, check if window.innerWidth is above or below 640px
+        // Crossing TO desktop
+        if (mobileNav && window.innerWidth > 640 && lastWidth <= 640) {
+            // Remove the .show class from mobile-nav
+            mobileNav.classList.remove('show');
+        }
+        // Crossing TO mobile
+        if (mobileNav && window.innerWidth < 640 && lastWidth >= 640) {
+            mobileNav.classList.remove('show');
+        }
+        lastWidth = window.innerWidth;
+
+        lastCheckTime = Date.now();
     }
-    if (window.innerWidth < 640 && lastWidth >= 640) {
-        console.log('CROSSED BACK!')
-    }
-    lastWidth = window.innerWidth;
 })
