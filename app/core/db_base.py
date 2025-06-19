61048100d69cf010f782bf66e6e91864d478e73d
isaacrosdail <idrfritcher@gmail.com>
2025-06-19 11:55:52 +0100

    Refactor CustomBase into Timestamp mixin & BaseModel, add automatic user.id FKey

--- app/core/db_base.py
+++ app/core/db_base.py
@@ -1,17 +1,14 @@
+# Defines our core Mixins as well as our BaseModel & declarative Base
+
 from sqlalchemy.orm import declarative_base, declared_attr
-from sqlalchemy import Column, Integer, DateTime, Boolean
+from sqlalchemy import Column, Integer, DateTime, ForeignKey
 from datetime import datetime, timezone
 from zoneinfo import ZoneInfo
 
-class CustomBase:
-    id = Column(Integer, primary_key=True) # Consider upping to UUIDs later
+# Core mixins
+class TimestampMixin:
     created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
     updated_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
-
-    # Now auto-gen tablenames (note: singular now ofc)
-    @declared_attr
-    def __tablename__(cls):
-        return cls.__name__.lower()
     
     # Applying our "virtual attribute" from Task model to our Base class here so any model can use it
     # @property is a common decorator for creating "virtual attributes" that are computed on-the-fly
@@ -26,8 +23,24 @@ class CustomBase:
     def updated_at_local(self):
         return self.updated_at.astimezone(ZoneInfo("Europe/London")) if self.updated_at else None
 
+# Base Model
+class BaseModel:
+    id = Column(Integer, primary_key=True) # Consider upping to UUIDs later
+
+    # Autogenerate table names (singular)
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+    
+    # Only add user_id FKey if this isn't the User model
+    @declared_attr
+    def user_id(cls):
+        if cls.__name__ != 'User':
+            return Column(Integer, ForeignKey('user.id'), nullable=False, default=1)
+
 # Define base class for models
-Base = declarative_base(cls=CustomBase)
+# Now all classes using (Base) receive IDs, a user.id FKey (except User), & Timestamp info
+Base = declarative_base(cls=BaseModel, mixins=(TimestampMixin,))
 
 # Mixin -> no Base, no CustomBase parent, just pure mixin
 # Will attach to HabitCompletion & Task for now (moving to TaskCompletion can be a potential task for future self!)
