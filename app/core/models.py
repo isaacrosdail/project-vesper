6386f57b7a7086ad9e02a3a04b4dd59beb26b2dc
isaacrosdail <idrfritcher@gmail.com>
2025-06-19 11:56:22 +0100

    Add User model (username only for now)

---
+++ app/core/models.py
@@ -0,0 +1,9 @@
+# Currently used to define our User model
+
+from sqlalchemy import Column, String
+from core.database import Base
+
+class User(Base):
+
+    username = Column(String(50), nullable=False, unique=True)
+    # add password_hash later
