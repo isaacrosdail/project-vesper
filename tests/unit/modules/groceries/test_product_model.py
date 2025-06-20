b9f237674a3d6c746b6513689bbd1cead08e3b06
isaacrosdail <idrfritcher@gmail.com>
2025-06-20 11:06:00 +0100

    Misc: Purge dumb tests

--- tests/unit/modules/groceries/test_product_model.py
+++ tests/unit/modules/groceries/test_product_model.py
@@ -1,16 +1,7 @@
+# Keep tests here to stuff like custom methods within models themselves?
 
 from app.modules.groceries.models import Product
 
-def test_product_creation():
-    product = Product(
-        barcode="12345",
-        product_name="Test Chips",
-        category="Snacks",
-        net_weight=100,
-        unit_type="g"
-    )
-    assert product.barcode == "12345"
-    assert product.product_name == "Test Chips"
 
 def test_product_string_representation():
     product = Product(product_name="Salsa", barcode="12345")
