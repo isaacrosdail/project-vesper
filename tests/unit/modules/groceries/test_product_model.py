a865036b8c896c612acb7b028622d806ccff245e
isaacrosdail <idrfritcher@gmail.com>
2025-06-20 11:06:00 +0100

    Remove dumb tests

--- tests/unit/modules/groceries/test_product_model.py
+++ tests/unit/modules/groceries/test_product_model.py
@@ -1,16 +1,6 @@
 
 from app.modules.groceries.models import Product
 
-def test_product_creation():
-    product = Product(
-        barcode="12345",
-        product_name="Test Chips",
-        category="Snacks",
-        net_weight=100,
-        unit_type="g"
-    )
-    assert product.barcode == "12345"
-    assert product.product_name == "Test Chips"
 
 def test_product_string_representation():
     product = Product(product_name="Salsa", barcode="12345")
--- tests/unit/modules/groceries/test_transaction_model.py
+++ tests/unit/modules/groceries/test_transaction_model.py
@@ -2,17 +2,6 @@
 from app.modules.groceries.models import Transaction
 from app.core.database import db_session
 
-def test_transaction_creation():
-    transaction = Transaction(
-        barcode="12345",
-        product_name="Test Chips",
-        category="Snacks",
-        net_weight=100,
-        unit_type="g"
-    )
-    assert transaction.barcode == "12345"
-    assert transaction.product_name == "Test Chips"
-
 def test_transaction_string_representation(sample_product):
     transaction = Transaction(
         product_id=sample_product.id,
