884cea1922c783f61a9f7a37a1f4f60934472f72
isaacrosdail <idrfritcher@gmail.com>
2025-06-22 18:41:44 +0100

    Refactor: Update fetch() requests to use async/await

--- app/static/js/utils.js
+++ app/static/js/utils.js
@@ -1,32 +1,37 @@
 // Currently used by tasks/dashboard & groceries/dashboard
 // DELETE fetch request when clicking delete button
-function deleteTableItem(module, itemId, subtype = "none") { // Default to none if not passed
+/**
+ * Deletes item from DB when clicking delete button
+ * @async
+ * @param {string} module 
+ * @param {number} itemId 
+ * @param {string} subtype 
+ * @returns 
+ */
+async function deleteTableItem(module, itemId, subtype = "none") { // Default to none if not passed
     // Confirm delete
     if (!confirm(`Are you sure you want to delete this item?`)) return;
 
     // Construct URL dynamically based on module & itemId
     const url = `/${module}/${subtype}/${itemId}`
 
-    // Fetch request to DELETE from db
-    // Remember: DELETE requests do not need a body nor a .then(data => ..)
-    fetch(url, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    })
-    .then(response => {
-        if (response.ok) {
-            // Handle success (eg., remove item from DOM)
-            // Need to grab the itemRow to remove visually by finding the
-            // custom data attribute data-task-id that matches our itemId
+    try {
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+        });
+        const responseData = await response.json();
+
+        if (responseData.success) {
+            // update DOM
             const itemRow = document.querySelector(`[data-item-id="${itemId}"]`);
             if (itemRow) itemRow.remove();
         } else {
-            console.error('Failed to delete item.');
+            // error from Flask route
+            console.error('Failed to delete item:', responseData.message);
         }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    })
+    } catch (error) {
+        // If fetch request as a whole failed (network/server errors)
+        console.error('Fetch request failed: ', error);
+    }
 }
