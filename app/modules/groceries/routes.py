934c49d69396e098c43315b864ca7fcb96beeea8
isaacrosdail <idrfritcher@gmail.com>
2025-06-26 23:19:04 +0100

    Remove old DELETE route, refactor

--- app/modules/groceries/routes.py
+++ app/modules/groceries/routes.py
@@ -1,13 +1,12 @@
 from decimal import Decimal
 
-from flask import Blueprint, flash, redirect, render_template, request, url_for
-
-from app.utils.sorting import bubble_sort
-from app.modules.groceries.utils import get_price_per_100g
-
-from app.core.database import db_session
+from app.core.database import database_connection
 from app.modules.groceries import models as grocery_models
 from app.modules.groceries import repository as grocery_repo
+from app.modules.groceries.utils import get_price_per_100g
+from app.utils.sorting import bubble_sort
+from flask import (Blueprint, flash, jsonify, redirect, render_template,
+                   request, url_for)
 
 groceries_bp = Blueprint('groceries', __name__, template_folder="templates", url_prefix="/groceries")
 
@@ -15,39 +14,39 @@ groceries_bp = Blueprint('groceries', __name__, template_folder="templates", url
 @groceries_bp.route("/dashboard", methods=["GET"])
 def dashboard():
 
-    session = db_session()
     try:
-        # Column names for Transactions model
-        transaction_column_names = [
-            grocery_models.Transaction.COLUMN_LABELS.get(col, col)
-            for col in grocery_models.Transaction.__table__.columns.keys()
-        ]
-        # Column names for Products model
-        product_column_names = [
-            grocery_models.Product.COLUMN_LABELS.get(col, col)
-            for col in grocery_models.Product.__table__.columns.keys()
-        ]
+        with database_connection() as session:
+            # Column names for Transactions model
+            transaction_column_names = [
+                grocery_models.Transaction.COLUMN_LABELS.get(col, col)
+                for col in grocery_models.Transaction.__table__.columns.keys()
+            ]
+            # Column names for Products model
+            product_column_names = [
+                grocery_models.Product.COLUMN_LABELS.get(col, col)
+                for col in grocery_models.Product.__table__.columns.keys()
+            ]
 
-        # Fetch products and transactions, pass into render_template
-        products = grocery_repo.get_all_products(session)
-        transactions = grocery_repo.get_all_transactions(session)
+            # Fetch products and transactions
+            products = grocery_repo.get_all_products(session)
+            transactions = grocery_repo.get_all_transactions(session)
 
-        # Compute price_per_100g using our util function
-        # Just tack that bad boy on there as a new attr
-        for transaction in transactions:
-            transaction.price_per_100g = get_price_per_100g(transaction)
+            # Compute price_per_100g using our util function
+            # Just tack that bad boy on there as a new attr
+            for transaction in transactions:
+                transaction.price_per_100g = get_price_per_100g(transaction)
 
-        # Sort transactions list by most recent DateTime first
-        bubble_sort(transactions, 'created_at', reverse=True)
-            
-        return render_template(
-            "groceries/dashboard.html", products = products,
-            transactions = transactions, 
-            product_column_names = product_column_names,
-            transaction_column_names = transaction_column_names
-        )
-    finally:
-        session.close()
+            # Sort transactions by most recent DateTime first
+            bubble_sort(transactions, 'created_at', reverse=True)
+                
+            return render_template(
+                "groceries/dashboard.html", products = products,
+                transactions = transactions, 
+                product_column_names = product_column_names,
+                transaction_column_names = transaction_column_names
+            )
+    except Exception as e:
+        return jsonify({"success": False, "message": str(e)}), 500
 
 @groceries_bp.route("/products", methods=["GET", "POST"])
 def products():
@@ -62,16 +61,15 @@ def products():
             "calories_per_100g": request.form.get("calories_per_100g")
         }
 
-        session = db_session()
         try:
-            grocery_repo.ensure_product_exists(session, **product_data)
-            session.commit()
-            # Flash message to confirm
-            flash("Product added successfully.")
+            with database_connection() as session:
+                grocery_repo.get_or_create_product(session, **product_data)
+                flash("Product added successfully.")    # Flash message to confirm
+                return redirect(url_for("groceries.dashboard"))
 
-            return redirect(url_for("groceries.dashboard"))
-        finally:
-            session.close()
+        except Exception as e:
+            return jsonify({"success": False, "message": str(e)}), 500
+        
     # GET => Show add_product form page
     else:
         return render_template("groceries/add_product.html")
@@ -85,12 +83,17 @@ def transactions():
         form_data = request.form.to_dict()
         # Bool to conditionally determine flash() message
         product_created = False
+
+        ######## TODO: EXTRACT THIS VALIDATION/ETC STUFF INTO HELPER FUNCTIONS
         # Normalize
         product_data = {
             # Use .get when the field might not be included at all (eg., in our "first pass" for a non-existent product here)
             "barcode": form_data.get("barcode", "").strip(),
             "product_name": form_data.get("product_name", "").strip(),
-            "net_weight": form_data.get("net_weight", "").strip()
+            "net_weight": form_data.get("net_weight", "").strip(),
+            "category": form_data.get("category", "").strip(),
+            "unit_type": form_data.get("unit_type", "").strip(),
+            "calories_per_100g": form_data.get("calories_per_100g", "").strip(),
         }
         transaction_data = {
             "price": form_data.get("price_at_scan", "").strip(),
@@ -122,43 +125,43 @@ def transactions():
                 transaction_data=form_data
             )
         
-        session = db_session()
         try:
-            # Check for product existence
-            product = grocery_repo.lookup_barcode(session, product_data["barcode"])
-        
-            # Product not found yet
-            # If product is missing: Check if net_weight is filled (ie., second form submit)
-            if not product:
-                if product_data["net_weight"] is None:
-                    # Not enough info yet - redisplay form asking for net_weight
-                    flash("Product not found. Please enter net weight.")
-                    return render_template(
-                        "groceries/add_transaction.html",
-                        show_product_fields=True,
-                        transaction_data=form_data
-                        )
-                else:
-                    # Now have enough info to add product
-                    grocery_repo.add_product(session, **product_data)
-                    product = grocery_repo.lookup_barcode(session, product_data["barcode"])
-        
-            # Product exists -> Add transaction & commit
-            grocery_repo.add_transaction(session, product, **transaction_data)
-            session.commit()
+            with database_connection() as session:
+                # Check for product existence
+                product = grocery_repo.lookup_barcode(session, product_data["barcode"])
+            
+                # Product not found yet
+                # If product is missing: Check if net_weight is filled (ie., second form submit)
+                if not product:
+                    if product_data["net_weight"] is None:
+                        # Not enough info yet - redisplay form asking for net_weight
+                        flash("Product not found. Please enter net weight.")
+                        return render_template(
+                            "groceries/add_transaction.html",
+                            show_product_fields=True,
+                            transaction_data=form_data
+                            )
+                    else:
+                        # Now have enough info to add product
+                        grocery_repo.add_product(session, **product_data)
+                        product = grocery_repo.lookup_barcode(session, product_data["barcode"])
+            
+                # Product exists -> Add transaction & commit
+                grocery_repo.add_transaction(session, product, **transaction_data)
 
-            # Flash message to confirm
-            flash("Transaction added successfully.")
+                flash("Transaction added successfully.") # flash confirmation
 
-            # Redirect logic based on user action submitted
-            action = request.form.get("action")
-            if action == "submit":
-                return redirect(url_for("groceries.dashboard"))
-            elif action == "next_item":
-                return redirect(url_for("groceries.add_transaction"))
-        finally:
-            session.close()
+                # Redirect logic based on user action submitted
+                action = request.form.get("action")
+                if action == "submit":
+                    return redirect(url_for("groceries.dashboard"))
+                elif action == "next_item":
+                    return redirect(url_for("groceries.add_transaction"))
+                
+        except Exception as e:
+            return jsonify({"success": False, "message": str(e)}), 500
 
+        return redirect(url_for("groceries.dashboard"))
     # GET
     else:
         barcode = request.args.get("barcode")
@@ -168,43 +171,3 @@ def transactions():
             show_product_fields=False, # Don't show add_product fields like net_weight by default
             transaction_data={} # For the "first" time add_transaction to prevent "undefined transaction_data"
         ) 
-    
-# DELETE (Product)
-# TODO: Improve JSON returns for success/error
-@groceries_bp.route("/products/<int:product_id>", methods=["DELETE"])
-def product(product_id):
-    session = db_session()
-    try:
-        product = session.get(grocery_models.Product, product_id) # Grab product by id from db
-
-        # If product doesn't exist
-        if not product:
-            return {"error": "Product not found."}, 404
-        
-        db_session.delete(product)
-        db_session.commit()
-        
-        return "", 204     # 204 means No Content (success but nothing to return, used for DELETEs)
-    finally:
-        session.close()
-
-# DELETE (Transaction)
-# TODO: Improve JSON returns for success/error
-@groceries_bp.route("/transactions/<int:transaction_id>", methods=["DELETE"])
-def transaction(transaction_id):
-    session = db_session()
-
-    try:
-        transaction = session.get(grocery_models.Transaction, transaction_id) # Grab transaction by id from db
-
-        # If product doesn't exist
-        if not transaction:
-            return {"error": "Transaction not found."}, 404
-        
-        db_session.delete(transaction)
-        db_session.commit()
-        
-        return "", 204     # 204 means No Content (success but nothing to return, used for DELETEs)
-    
-    finally:
-        session.close()
