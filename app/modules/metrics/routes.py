f928e57854870a04528cb2e54c4e62793e847009
isaacrosdail <idrfritcher@gmail.com>
2025-06-27 12:48:39 +0100

    Refactor: use context manager, extract magic number

--- app/modules/metrics/routes.py
+++ app/modules/metrics/routes.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, request, jsonify
 from app.core.database import db_session, database_connection
 from app.modules.habits.models import DailyMetric, DailyCheckin
 
-from app.modules.metrics.utils import get_metric_dataframe, create_metric_chart_html
+from app.utils.visualization.charts import get_metric_dataframe, create_metric_chart_html
 
 # New Blueprint
 metrics_bp = Blueprint('metrics', __name__, template_folder='templates', url_prefix='/metrics')
@@ -12,19 +12,22 @@ metrics_bp = Blueprint('metrics', __name__, template_folder='templates', url_pre
 @metrics_bp.route('/dashboard', methods=["GET"])
 def dashboard():
 
-    # # Takes in metric_type str & days_ago int => returns DataFrame
-    # # Get DataFrame for metric_type weight starting from 7 days ago (ie, show last 7 days)
-    df = get_metric_dataframe("weight", 7)
-    # # Pass in df, metric_type => returns graph_html figure
-    weight_graph = create_metric_chart_html(df, "weight")
+    DEFAULT_DAYS = 14
 
-    # Same for steps
-    df_2 = get_metric_dataframe("steps", 7)
-    steps_graph = create_metric_chart_html(df_2, "steps")
+    with database_connection() as session:
+        # # Takes in metric_type str & days_ago int => returns DataFrame
+        # # Get DataFrame for metric_type weight starting from 7 days ago (ie, show last 7 days)
+        df = get_metric_dataframe("weight", DEFAULT_DAYS, session)
+        # # Pass in df, metric_type => returns graph_html figure
+        weight_graph = create_metric_chart_html(df, "weight")
 
-    return render_template("metrics/dashboard.html", 
-                           weight_graph=weight_graph,
-                           steps_graph=steps_graph)
+        # Same for steps
+        df_2 = get_metric_dataframe("steps", DEFAULT_DAYS, session)
+        steps_graph = create_metric_chart_html(df_2, "steps")
+
+        return render_template("metrics/dashboard.html", 
+                            weight_graph=weight_graph,
+                            steps_graph=steps_graph)
 
 
 @metrics_bp.route("/", methods=["POST"])
