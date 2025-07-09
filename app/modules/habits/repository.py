d7b69ca82e625e40b375ef1008ed4356d3122885
isaacrosdail <idrfritcher@gmail.com>
2025-07-09 13:45:58 -0500

    Move repo function

--- app/modules/habits/repository.py
+++ app/modules/habits/repository.py
@@ -1,20 +1,7 @@
 # DB logic functions to access data
 
-from .models import Habit, HabitCompletion, DailyIntention
-from sqlalchemy import func
-from datetime import datetime, timezone
+from .models import Habit, HabitCompletion
+
 # Get all habits
 def get_all_habits(session):
     return session.query(Habit).all()
-
-# Get today's daily intention
-def get_today_intention(session):
-
-    # Get today in UTC
-    today_utc = datetime.now(timezone.utc).date()
-
-    todayIntention = session.query(DailyIntention).filter(
-        func.date(DailyIntention.created_at) == today_utc
-    ).first()
-    
-    return todayIntention
