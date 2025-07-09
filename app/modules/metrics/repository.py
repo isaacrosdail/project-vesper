01b1d3f19650ba25c9a22eee4f1f9b4383ef6491
isaacrosdail <idrfritcher@gmail.com>
2025-07-09 13:45:58 -0500

    Move repo function

---
+++ app/modules/metrics/repository.py
@@ -0,0 +1,17 @@
+
+from .models import DailyMetric, DailyIntention
+from sqlalchemy import func
+from datetime import datetime, timezone
+
+def get_all_metrics(session):
+    return session.query(DailyMetric).all()
+
+def get_today_intention(session):
+    # Get today in UTC
+    today_utc = datetime.now(timezone.utc).date()
+
+    todayIntention = session.query(DailyIntention).filter(
+        func.date(DailyIntention.created_at) == today_utc
+    ).first()
+    
+    return todayIntention
