2d2d8e6fc33567cec14ac4d88fd07a3b64a41c8c
isaacrosdail <idrfritcher@gmail.com>
2025-07-30 23:46:50 -0500

    Toying around with error messages?
    - Fix import statement

---
+++ app/core/constants.py
@@ -0,0 +1,6 @@
+# Centralizing some constants for sanity? We'll see how this goes and pivot later
+# Really not sure about this :D
+
+MIN_PASSWORD_LENGTH = 8
+MAX_PASSWORD_LENGTH = 14
+MAX_NAME_LENGTH = 50
