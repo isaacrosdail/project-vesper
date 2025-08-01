549105a8494ef97bc79d33e26d37eceeabc6243c
isaacrosdail <idrfritcher@gmail.com>
2025-07-31 21:32:36 -0500

    Make password_hash nullable, use default hashing method for gen password hash

--- app/core/auth/models.py
+++ app/core/auth/models.py
@@ -1,21 +1,20 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.core.db_base import Base
 from sqlalchemy import Column, String
+from flask_login import UserMixin
 
-
-class User(Base):
+class User(Base, UserMixin):
 
     username = Column(String(50), nullable=False, unique=True)
     name = Column(String(50), nullable=False)
     password_hash = Column(String(256), nullable=False) # Large enough? bcrypt = 60chars, Werkzeug's default uses pbkdf2:sha256 = around 95 chars
-    role = Column(String(50), nullable=False)
+    role = Column(String(50), nullable=False, default="user")
 
     # Instance methods
     def set_password(self, plaintext: str) -> None:
         """Hash + salt + store."""
         self.password_hash = generate_password_hash(
             plaintext,
-            method="pdkf2:sha256", # default, explicit here for clarity
             salt_length=16
         )
     
