ce74e5f688c9809726b8b9d90100867a275f9fb6
isaacrosdail <idrfritcher@gmail.com>
2025-08-02 19:49:06 -0500

    Refactor: accommodate optional reset of users, extract lists to constants.py

--- app/common/database/operations.py
+++ app/common/database/operations.py
@@ -2,35 +2,27 @@
 # Exact intention of this file TBD
 
 from sqlalchemy import text
+from app.core.constants import DATA_TABLES, ALL_TABLES, DATA_SEQUENCES, ALL_SEQUENCES
 
 # Delete all data without nuking schema, optionally reset ID sequencing
 # Does NOT delete from User table
-def delete_all_db_data(engine, reset_sequences=False):
-    # Delete in dependency order -> children first
-    tables = [
-        "habitcompletion",   # child: references habit
-        "transaction",       # child: references product
-        "habit",             # parent table
-        "product",           # parent table
-        "task"               # independent
-    ]
-    sequences = [
-        "habitcompletion_id_seq",  # references habit
-        "transaction_id_seq",      # references product
-        "habit_id_seq",            # parent table
-        "product_id_seq",  # parent table
-        "task_id_seq"
-    ]
+def delete_all_db_data(engine, reset_sequences=False, include_users=False):
+    """Delete all database data, with flags for also deleting users."""
+
+    tables = ALL_TABLES if include_users else DATA_TABLES
+    sequences = ALL_SEQUENCES if include_users else DATA_SEQUENCES
+
     # New way with Alembic instead of create_all()
     with engine.begin() as conn:
         # Delete data
         for table in tables:
             conn.execute(text(f"DELETE FROM {table}"))
 
-            # Reset sequences if desired
-            if reset_sequences:
-                for seq in sequences:
-                    conn.execute(text(f"ALTER SEQUENCE {seq} RESTART WITH 1"))
+        # Reset sequences if desired
+        if reset_sequences:
+            for seq in sequences:
+                conn.execute(text(f"ALTER SEQUENCE {seq} RESTART WITH 1"))
+
 
 # TODO: Implement soft delete for DB entries, then use this function for handling our soft delete for products
 def soft_delete():
