18d8eaf81e49056e5555ce762cc0a62e29faac24
isaacrosdail <idrfritcher@gmail.com>
2025-08-03 16:47:32 -0500

    Refactor: Update tests to match 'return errors list' pattern

--- tests/unit/modules/groceries/test_groceries_validate.py
+++ tests/unit/modules/groceries/test_groceries_validate.py
@@ -15,13 +15,16 @@ def test_add_product_missing_data_raises_error():
         "net_weight": 200
     }
 
-    # Ensure ValueError is raised
-    with pytest.raises(ValueError) as excinfo:
-        validate_product_data(incomplete_data)
-    
-    assert "Product name is required" in str(excinfo.value)
+    errors = validate_product_data(incomplete_data)
+    # # Ensure ValueError is raised
+    # with pytest.raises(ValueError) as excinfo:
+    #     validate_product_data(incomplete_data)
+    # assert "Product name is required" in str(excinfo.value)
+    assert len(errors) == 3 # (missing product_name, category, unit_type)
 
-
-def test_add_transaction_requires_product(logged_in_user):
-    with pytest.raises(ValueError, match="Product must be provided for transaction."):
-        grocery_repo.add_transaction(db_session, None, logged_in_user.id, price="2.50", quantity=1)
+def test_add_transaction_requires_product():
+    #with pytest.raises(ValueError, match="Product must be provided for transaction."):
+    transaction_data = {
+        "product_name"
+    }
+    validate_transaction_data(db_session, None, price_at_scan="2.50", quantity=1)
