332864a5a8f20531ad37d727e62379aee51895e5
isaacrosdail <idrfritcher@gmail.com>
2025-08-03 16:37:00 -0500

    Add Docstring for get_user_tasks

--- app/modules/tasks/repository.py
+++ app/modules/tasks/repository.py
@@ -9,4 +9,12 @@ def get_all_tasks(session):
 
 # Get all tasks for given user_id
 def get_user_tasks(session, user_id):
+    """
+    Get all task entries for given user id.
+    Args:
+        session: SQLAlchemy Session object.
+        user_id: User id to filter by.
+    Returns:
+        List of Task objects.
+    """
     return session.query(Task).filter(Task.user_id==user_id).all()
