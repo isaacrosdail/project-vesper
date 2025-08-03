401ac45295eec37805541630bf0af0f19f46e0d6
isaacrosdail <idrfritcher@gmail.com>
2025-08-03 12:45:32 -0500

    Refactor: auth: make user-aware with filtering by user_id

--- app/modules/habits/repository.py
+++ app/modules/habits/repository.py
@@ -1,7 +1,18 @@
 # DB logic functions to access data
 
 from .models import Habit, HabitCompletion
+from datetime import datetime, timezone
+from sqlalchemy import func
 
-# Get all habits
-def get_all_habits(session):
-    return session.query(Habit).all()
+
+# Get all habits for given user id
+def get_user_habits(session, user_id):
+    return session.query(Habit).filter(Habit.user_id==user_id).all()
+
+# Get today's habit completions for a given user id
+def get_user_today_habit_completions(session, user_id, habit_id, date):
+    return session.query(HabitCompletion).join(Habit).filter(
+        HabitCompletion.habit_id == habit_id,
+        Habit.user_id == user_id,
+        func.date(HabitCompletion.created_at) == date
+    ).first()
