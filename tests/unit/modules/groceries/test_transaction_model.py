22c44a809051f789b836a24e36d866740af94b54
isaacrosdail <idrfritcher@gmail.com>
2025-08-03 16:48:03 -0500

    Refactor: auth: Add user-scoping & use authenticated_client fixture for tests

--- tests/unit/modules/groceries/test_transaction_model.py
+++ tests/unit/modules/groceries/test_transaction_model.py
@@ -3,11 +3,12 @@
 from app.modules.groceries.models import Transaction
 from app.core.database import db_session
 
-def test_transaction_string_representation(sample_product):
+def test_transaction_string_representation(sample_product, logged_in_user):
     transaction = Transaction(
         product_id=sample_product.id,
         price_at_scan=4.99, 
         quantity=2,
+        user_id=logged_in_user.id
     )
     db_session.add(transaction)
     db_session.flush()
