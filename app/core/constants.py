45b94660aa3c9336082552a2193d904bf6b317e9
isaacrosdail <idrfritcher@gmail.com>
2025-08-02 19:15:42 -0500

    Add DEFAULT_LANG to en

--- app/core/constants.py
+++ app/core/constants.py
@@ -1,6 +1,7 @@
 # Centralizing some constants for sanity? We'll see how this goes and pivot later
 # Really not sure about this :D
 
+DEFAULT_LANG = "en"
 MIN_PASSWORD_LENGTH = 8
 MAX_PASSWORD_LENGTH = 14
 MAX_NAME_LENGTH = 50
