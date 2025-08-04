a2d0af64f9fca07bba2e0740d0f5121a81d2b1cb
isaacrosdail <idrfritcher@gmail.com>
2025-08-03 22:49:07 -0500

    Add now_local helper to get timezone-aware datetime using IANA timezone string

--- app/common/datetime/helpers.py
+++ app/common/datetime/helpers.py
@@ -1,7 +1,24 @@
 # Grabbing certain times / formatting datetimes
 
 from datetime import datetime, timezone
+from zoneinfo import ZoneInfo, ZoneInfoNotFoundError
 
 
 def now_utc():
     return datetime.now(timezone.utc)
+
+def now_local(timezone_str: str = 'UTC') -> datetime:
+    """
+    Get current time in specified timezone.
+    
+    Args:
+        timezone_str: Timezone string (eg, 'US/Central', 'UTC')
+    Returns:
+        Timezone-aware datetime object
+    """
+    try:
+        # Make tz object
+        tz = ZoneInfo(timezone_str)
+        return datetime.now(tz) # current time in that timezone
+    except ZoneInfoNotFoundError:
+        raise ValueError(f"Invalid timezone: {timezone_str}")
