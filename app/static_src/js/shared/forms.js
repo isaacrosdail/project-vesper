a0e5d15f3367efed93c2221fb205628079921eca
isaacrosdail <idrfritcher@gmail.com>
2025-09-23 21:57:19 -0500

    forms: login/register: add initPasswordToggle()/styling for toggle password visibility

--- app/static_src/js/shared/forms.js
+++ app/static_src/js/shared/forms.js
@@ -46,4 +46,23 @@ export function initProductForms() {
     });
 }
 
+export function initPasswordToggles() {
+    const toggleButtons = document.querySelectorAll('[data-password-toggle]');
+    if (toggleButtons.length === 0) return;
+
+    toggleButtons.forEach(btn => {
+        const inputId = btn.dataset.passwordToggle;
+        const input = document.querySelector(`#${inputId}`);
+        if (!input) return;
+        btn.setAttribute('aria-pressed', input.type === 'text' ? 'true' : 'false')
+
+        btn.addEventListener('click', () => {
+            const isPressed = btn.getAttribute('aria-pressed') === 'true';
+            input.type = isPressed ? 'password' : 'text';
+            btn.setAttribute('aria-pressed', String(!isPressed));
+        })
+    })
+}
+
 initProductForms();
+initPasswordToggles();
