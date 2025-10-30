ce9a76aa41c407673719c41066fb9978b2372b6b
isaacrosdail <idrfritcher@gmail.com>
2025-10-29 19:01:18 -0500

    fix(ui): center tooltip-popup under target element

--- app/static_src/js/shared/ui/tooltip.js
+++ app/static_src/js/shared/ui/tooltip.js
@@ -8,27 +8,26 @@
  */
 
 export function showToolTip(targetEl, textContent) {
-    // Get button's position on screen
     const tooltip = document.createElement('div');
     tooltip.id = 'tooltip';
     tooltip.className = 'tooltip-popup';
     tooltip.setAttribute('role', 'tooltip'); // For a11y
     tooltip.setAttribute('aria-hidden', 'false');
     targetEl.setAttribute('aria-describedby', tooltip.id); // Needs to be on trigger, not tooltip itself
-    tooltip.textContent = textContent; // get text from HTML's data attr
+    tooltip.textContent = textContent;
 
-    const tooltipPos = targetEl.getBoundingClientRect(); // gives us position info for triggers
-    const isTall = tooltipPos.height > 80;
-    const buttonCenterX = (tooltipPos.left + tooltipPos.right) / 2; // gives us dist from end to center for carrot
-    const y = isTall ? (tooltipPos.top + 2 * (tooltipPos.height / 3)) : tooltipPos.bottom;
+    const targetElRect = targetEl.getBoundingClientRect(); // gives us position info for triggers
+    const isTall = targetElRect.height > 80;
+    const targetElCenterX = (targetElRect.left + targetElRect.right) / 2; // gives us dist from end to center for carrot
+    const y = isTall ? (targetElRect.top + 2 * (targetElRect.height / 3)) : targetElRect.bottom;
     tooltip.style.position = 'fixed';
-    tooltip.style.left = tooltipPos.left + 'px';
     tooltip.style.top = `${y}px`;
     tooltip.style.zIndex = '1000'; // TODO: Consider dropping to 100-199 range to adhere to z-index notes in app.css
     document.body.appendChild(tooltip);
 
     const tooltipRect = tooltip.getBoundingClientRect();
-    let arrowX = buttonCenterX - tooltipRect.left; // calc carrot position inside tooltip
+    tooltip.style.left = `${targetElCenterX - (tooltipRect.width / 2)}px`;
+    let arrowX = (tooltipRect.width / 2); // calc carrot position inside tooltip
     tooltip.style.setProperty('--carrot-pos', `${arrowX}px`);
 }
 
