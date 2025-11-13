68af632702874c046087a0feaeac9317e0ec5b00
isaacrosdail <idrfritcher@gmail.com>
2025-11-13 16:32:20 -0600

    refactor(D3 stuff): standardize chart containers & add empty dataset/state handling
    - Rename chart container IDs to follow pattern: [module]-chart-container
    - Add empty state messages when dataset for chosen timeframe is empty
    - Add/Standardize chart range options across modules: 1d, 7d, 30d
    - Add table range selectors for all habits, metrics, & time_tracking tables
    - Refactor chart layouts to use consistent margin/transform structure

--- app/static_src/js/habits/dashboard.ts
+++ app/static_src/js/habits/dashboard.ts
@@ -8,89 +8,125 @@ type BarData = {
     count: number;
 }
 
-type BarEnterSelection = d3.Selection<d3.EnterElement, BarData, SVGGElement, unknown>;
-type BarUpdateSelection = d3.Selection<SVGRectElement, BarData, SVGGElement, unknown>;
-type BarExitSelection = d3.Selection<SVGRectElement, BarData, SVGGElement, unknown>;
+const chartState = {
+    range: 7,
+}
 
 // Dimensions
 const margin = { top: 20, right: 30, bottom: 40, left: 90 };
-const width = 350 - margin.left - margin.right;
-const height = 350 - margin.top - margin.bottom;
+const width = 400;
+const height = 300;
+const innerWidth = width - margin.left - margin.right;
+const innerHeight = height - margin.top - margin.bottom;
 
 // Set up chart svg as a whole
-const svg = d3.select(".chart-container")
+const svg = d3.select("#habits-chart-container")
     .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-    .append("g")
+        .attr("width", width)
+        .attr("height", height)
+
+const gRoot = svg.append("g")
         .attr("transform", `translate(${margin.left}, ${margin.top})`)
 
 const title = svg.append("text")
-    .attr("x", (width/2))
-    .attr("y", -margin.top/2)
+    .attr("x", innerWidth/2)
+    .attr("y", margin.top/2)
     .attr("font-size", "var(--font-size-l)")
     .text("Completions by Habit")
 
-const gXAxis = svg.append("g")
+const gXAxis = gRoot.append("g")
     .attr("class", "axis-x")
-    .attr("transform", `translate(0, ${height})`);
-const gYAxis = svg.append("g")
+    .attr("transform", `translate(0, ${innerHeight})`);
+const gYAxis = gRoot.append("g")
     .attr("class", "axis-y");
+const gChart = gRoot.append("g")
+    .attr("class", "chart");
 
-const xScale = d3.scaleLinear().range([0, width]);
-const yScale = d3.scaleBand().range([0, height]).padding(0.2);
+const xScale = d3.scaleLinear().range([0, innerWidth]);
+const yScale = d3.scaleBand().range([0, innerHeight]).padding(0.2);
 
 function updateBarChart(data: BarData[]) {
+
+    gChart.selectAll(".empty-message").remove();
+
     // Update axes
-    xScale.domain([0, d3.max(data, (d: BarData) => d.count)])
+    xScale.domain([0, d3.max(data, (d: BarData) => d.count)!])
     yScale.domain(data.map(d => d.name))
 
     gXAxis.call(
         d3.axisBottom(xScale)
-            .tickValues(d3.range(0, d3.max(data, (d: BarData) => d.count) + 1, 1))
+            .tickValues(d3.range(0, d3.max(data, (d: BarData) => d.count)! + 1, 1))
         )
     gYAxis.call(d3.axisLeft(yScale));
 
-    const bars = svg.selectAll("rect")
+    const bars = gChart.selectAll("rect.bar")
     .data(data)
     .join(
-        (enter: BarEnterSelection) => {
-            return enter.append("rect")
+        enter => {
+            const rects = enter.append("rect")
                 .attr("x", 0)
-                .attr("y", (d: BarData) => yScale(d.name))
+                .attr("y", (d: BarData) => yScale(d.name)!)
                 .attr("width", 0)
                 .attr("height", yScale.bandwidth())
                 .attr("fill", "var(--accent-strong)")
-                .attr("class", "line")
-                .transition()
-                .duration(500)
+                .attr("class", "bar")
+            
+            rects.transition()
+                .duration(200)
                 .attr("width", (d: BarData) => xScale(d.count))
+
+            return rects
         },
-        (update: BarUpdateSelection) => update
+        update => update
             .transition()
             .duration(200)
-            .attr("width", (d: BarData) => xScale(d.count)),
-        (exit: BarExitSelection) => exit.remove()
+            .attr("width", (d: BarData) => xScale(d.count))
+            .attr("height", yScale.bandwidth()),
+        exit => exit.remove()
     );
 
-    bars.on('mouseenter', function(this: SVGRectElement, _event: any, d: BarData) {
-        showToolTip(this, `${d.name}: ${d.count}`);
-    })
-    .on('mouseleave', function() {
-        hideToolTip();
-    });
+    bars.on('mouseenter', function(this: d3.BaseType, _event: any, d: BarData) {
+        showToolTip(this as SVGRectElement, `${d.name}: ${d.count}`);
+    }).on('mouseleave', hideToolTip);
+}
+
+function showEmptyChart() {
+    gChart.selectAll('rect.bar').remove();
+
+    const emptyMessage = gChart.selectAll('text.empty-message')
+        .data([1])
+        .join("text")
+        .attr("class", "empty-message")
+        .attr("x", innerWidth/2)
+        .attr("y", innerHeight/2)
+        .attr("text-anchor", "middle")
+        .attr("fill", "grey")
+        .text("No habit completion data for this period.")
+}
+
+async function refreshBarChart() {
+    const data = await getHabitsData(chartState.range);
+    console.log(data)
+    if (data.length === 0) {
+        showEmptyChart();
+        return;
+    }
+    updateBarChart(data);
 }
 
 export async function init() {
-    const data = await getHabitsData(1);
-    updateBarChart(data);
+    await refreshBarChart();
 
     document.addEventListener('click', async (e) => {
         const target = e.target as HTMLElement;
-        if (target.matches('.timeframe-selection')) {
-            const range = parseInt(target.dataset['range']!, 10);
-            const newData = await getHabitsData(range);
-            updateBarChart(newData);
+        
+        if (target.matches('.chart-range')) {
+            chartState.range = parseInt(target.dataset['range']!, 10);
+            await refreshBarChart();
+        }
+        else if (target.matches('.table-range')) {
+            const range = target.dataset['range']!;
+            window.location.href = `/habits/dashboard?range=${range}`;
         }
     });
 }
