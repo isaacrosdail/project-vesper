d07f3e8b101e505d4a69890eca2bdd00dcfe632f
isaacrosdail <idrfritcher@gmail.com>
2025-11-13 16:32:20 -0600

    refactor(D3 stuff): standardize chart containers & add empty dataset/state handling
    - Rename chart container IDs to follow pattern: [module]-chart-container
    - Add empty state messages when dataset for chosen timeframe is empty
    - Add/Standardize chart range options across modules: 1d, 7d, 30d
    - Add table range selectors for all habits, metrics, & time_tracking tables
    - Refactor chart layouts to use consistent margin/transform structure

--- app/modules/habits/routes.py
+++ app/modules/habits/routes.py
@@ -5,12 +5,13 @@ from typing import TYPE_CHECKING, Any
 if TYPE_CHECKING:
     from sqlalchemy.orm import Session
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import current_user
 
 from app.modules.habits.repository import HabitsRepository
 from app.modules.habits.viewmodels import HabitPresenter, HabitViewModel
 from app.shared.decorators import login_plus_session
+from app.shared.datetime.helpers import last_n_days_range
 
 
 habits_bp = Blueprint('habits', __name__, template_folder="templates", url_prefix="/habits")
@@ -20,11 +21,15 @@ habits_bp = Blueprint('habits', __name__, template_folder="templates", url_prefi
 @login_plus_session
 def dashboard(session: 'Session') -> Any:
     
+    selected_range = request.args.get("range", 7, type=int)
+
     habits_repo = HabitsRepository(session, current_user.id, current_user.timezone)
-    habits = habits_repo.get_all_habits_and_tags()
+    start_utc, end_utc = last_n_days_range(selected_range, current_user.timezone)
+    habits = habits_repo.get_all_habits_and_tags_in_window(start_utc, end_utc)
 
     viewmodels = [HabitViewModel(h, current_user.timezone) for h in habits]
     ctx = {
+        "selected_range": selected_range,
         "headers": HabitPresenter.build_columns(),
         "habits": viewmodels
     }
--- app/modules/time_tracking/routes.py
+++ app/modules/time_tracking/routes.py
@@ -4,14 +4,14 @@ from typing import TYPE_CHECKING, Any
 if TYPE_CHECKING:
     from sqlalchemy.orm import Session
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import current_user
 
 from app.modules.time_tracking.repository import TimeTrackingRepository
 from app.modules.time_tracking.service import TimeTrackingService
 from app.modules.time_tracking.viewmodels import (TimeEntryPresenter,
                                                   TimeEntryViewModel)
-from app.shared.datetime.helpers import today_range_utc, now_in_timezone
+from app.shared.datetime.helpers import now_in_timezone, last_n_days_range
 from app.shared.sorting import bubble_sort
 from app.shared.decorators import login_plus_session
 
@@ -22,16 +22,19 @@ time_tracking_bp = Blueprint('time_tracking', __name__, template_folder='templat
 @time_tracking_bp.route('/dashboard', methods=["GET"])
 @login_plus_session
 def dashboard(session: 'Session') -> Any:
+
+    range_days = request.args.get('range', 7, type=int)
+
     repo = TimeTrackingRepository(session, current_user.id, current_user.timezone)
-    start_utc, end_utc = today_range_utc(current_user.timezone)
-    #entries = repo.get_all_time_entries_in_window(start_utc, end_utc)
-    time_entries = repo.get_all()
+    start_utc, end_utc = last_n_days_range(range_days, current_user.timezone)
+    time_entries = repo.get_all_time_entries_in_window(start_utc, end_utc)
     bubble_sort(time_entries, 'started_at', reverse=True)
     current_date = now_in_timezone(current_user.timezone).date().isoformat()
 
     viewmodels = [TimeEntryViewModel(e, current_user.timezone) for e in time_entries]
     ctx = {
         "entries": viewmodels,
+        "selected_range": range_days,
         "entry_headers": TimeEntryPresenter.build_columns(),
         "current_date": current_date,
     }
