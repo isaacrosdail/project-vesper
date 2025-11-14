527fef0d1be4327c9f886ab6a80f487da401cd04
isaacrosdail <idrfritcher@gmail.com>
2025-11-14 14:45:50 -0600

    chore(TS): improve typing, lint, purge unused imports/comments
    - Remove unused imports
    - Improve typing & fix linter warnings
    - datetimes.ts: Remove unused functions
    - api.ts: Add JSDoc, clean comments

--- app/static_src/js/shared/datetime.ts
+++ app/static_src/js/shared/datetime.ts
@@ -1,11 +1,4 @@
-// Replicating our Python datetime/helpers.py
-
-// TODO: NOTES: Dependency injection
 import { userStore } from './services/userStore.js';
-
-function nowUTC(): string {
-    return new Date().toISOString(); // Returns "2025-08-17T15:02:33.022Z"
-}
 
 export const getJSInstant = (): string =>
     new Date().toISOString(); // Always UTC "..Z"
@@ -42,8 +35,4 @@ export function formatToUserTimeString(
         timeZone: (userStore.state === 'loaded' && userStore.data?.timezone) ? userStore.data.timezone : 'UTC',
     });
     return formatter.format(date); // eg, "03:45"
-}
-
-function padTime(number: number): string {
-    return number.toString().padStart(2, '0');
 }
