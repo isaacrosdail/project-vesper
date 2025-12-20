d242aa39ec9801e89b69aaabad43f77623daf992
isaacrosdail <idrfritcher@gmail.com>
2025-12-20 14:02:23 -0600

    chore: add linting/formatting tooling, pre-push/commit hooks with linting
    - Install stylelint for CSS, djLint for Jinja
    - Add pyproject.toml for ruff, mypy, pytest, & djlint (& remove old .ini's)
    - Add stylelint config with recess-order for CSS
    
    - Update pre-commit hook to run TS, Python, CSS, & template linting
    - Add pre-push hook to run tests and security audits
    - Add stylelint with standard config (mostly) and recess order
    - Update package.json scripts for modular linting/testing

---
+++ stylelint.strict.mjs
@@ -0,0 +1,14 @@
+/** @type {import("stylelint").Config} */
+export default {
+    "extends":
+    [
+        "stylelint-config-standard",
+        "stylelint-config-recess-order"
+    ],
+    "rules": {
+        "rule-empty-line-before": null,
+        "at-rule-empty-line-before": null,
+        "selector-class-pattern": "^([a-z][a-z0-9]*)(-[a-z0-9]+)*(--[a-z0-9-]+)?$", /* Allow BEM modifiers */
+        "property-no-vendor-prefix": null,
+    }
+};
