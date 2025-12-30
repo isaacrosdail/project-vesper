28bafd0fb0b6ea638e33b2bcca2c60cb491e4be0
isaacrosdail <idrfritcher@gmail.com>
2025-12-29 19:59:59 -0600

    refactor(dashboard.html/respository/routes): Groceries dashboard improvements
    - Add include_soft_deleted param to show all products in dropdown
    - Implement Hungarian notation (l_, d_, etc) for Jinja template variables
    - Clean up route variable naming (product_for_dropdown, products_for_table)
    - Add `include_soft_deleted` parameter to both get_all_products*() repo functions

--- app/modules/groceries/routes.py
+++ app/modules/groceries/routes.py
@@ -33,24 +33,39 @@ def dashboard(session: 'Session') -> Any:
     start_utc, end_utc = last_n_days_range(transactions_params['range'], current_user.timezone)
     transactions = groceries_repo.get_all_transactions_in_window(start_utc, end_utc)
     transactions = sort_by_field(transactions, transactions_params['sort_by'], transactions_params['order'])
-    txn_viewmodels = [TransactionViewModel(t, current_user.timezone) for t in transactions]
+    transactions_for_table = [TransactionViewModel(t, current_user.timezone) for t in transactions]
 
     products_params = get_table_params('products', 'name')
     start_utc, end_utc = last_n_days_range(products_params['range'], current_user.timezone)
-    products = groceries_repo.get_all_products_in_window(start_utc, end_utc)
-    products = sort_by_field(products, products_params['sort_by'], products_params['order'])
-    prod_viewmodels = [ProductViewModel(p, current_user.timezone) for p in products]
+
+    products_for_dropdown= groceries_repo.get_all_products(include_soft_deleted=True)
+    products_for_dropdown= sort_by_field(products_for_dropdown, 'name', 'asc')
+
+    active_products_for_table = groceries_repo.get_all_products_in_window(start_utc, end_utc)
+    active_products_for_table = sort_by_field(
+        active_products_for_table, 
+        products_params['sort_by'], 
+        products_params['order']
+    )
+
+    products_for_table = [ProductViewModel(p, current_user.timezone) for p in active_products_for_table]
 
     shopping_list, _ = groceries_service.get_or_create_shoppinglist()
 
+    category_options = [
+        { 'value': enum_member.value.lower(), 'text': display_string}
+        for enum_member, display_string in ProductViewModel.CATEGORY_LABELS.items()
+    ]
+
     ctx = {
-        "transactions_params": transactions_params,
-        "transaction_headers": TransactionPresenter.build_columns(),
-        "transactions": txn_viewmodels,
-        "txn_form_products_list": products,
-        "products_params": products_params,
-        "product_headers": ProductPresenter.build_columns(),
-        "products": prod_viewmodels,
-        "shopping_list": shopping_list,
+        "d_transactions_params": transactions_params,
+        "l_transaction_headers": TransactionPresenter.build_columns(),
+        "l_transactions_for_table": transactions_for_table,
+        "l_products_for_dropdown":  products_for_dropdown,
+        "d_products_params": products_params,
+        "l_product_headers": ProductPresenter.build_columns(),
+        "l_products_for_table": products_for_table,
+        "o_shopping_list": shopping_list,
+        "l_category_options": category_options,
     }
     return render_template("groceries/dashboard.html", **ctx)
