9f87ed495e209429b7e7978fb2c768c5b6c26ed6
isaacrosdail <idrfritcher@gmail.com>
2025-12-30 19:43:23 -0600

    fix: tooltip positioning in dialogs/modals
    - Append tooltips to dialog ancestor when present (absolute positioning)
    - Non-dialog tooltips still append to body with fixed positioning
    - Fixes stacking context issue causing tooltips to appear beneath dialogs
    - Fix subtle bug caused by tooltip style assignment order
    - Rename show/hide functions to create/remove for accuracy
    - Update JSDocs
    - Also: Move data-tip from task name span to its parent td for more consistent tooltip behavior

--- app/static_src/js/shared/ui/tooltip.ts
+++ app/static_src/js/shared/ui/tooltip.ts
@@ -2,12 +2,17 @@
 // Custom tooltip behavior
 
 /**
- * To use for a given element:
- * - `.tooltip` for styling, `data-tip` for behavior
- * - Uses passed text if provided, otherwise falls back to `data-tip`
- * - Add 'data-tip' attr with desired tooltip text
+ * Create & display a tooltip anchored under a given element.
+ * 
+ * Caller is responsible for subsequently removing tooltip via `removeTooltip()`.
+ * 
+ * @param targetEl - Element to which to attach tooltip.
+ * @param tooltipText - Tooltip text (falls back to `data-tip` attribute value if not provided)
+ * 
+ * @remarks
+ * Styling via `.tooltip` class, behavior via `data-tip` attribute
  */
-export function showTooltip(targetEl: HTMLElement | SVGElement, tooltipText?: string): void {
+export function createTooltip(targetEl: HTMLElement | SVGElement, tooltipText?: string): void {
     const text = tooltipText || targetEl.getAttribute('data-tip');
     if (!text) {
         console.warn('No tooltip text provided');
@@ -19,26 +24,49 @@ export function showTooltip(targetEl: HTMLElement | SVGElement, tooltipText?: st
     tooltip.className = 'tooltip';
     tooltip.setAttribute('role', 'tooltip'); // For a11y
     tooltip.setAttribute('aria-hidden', 'false');
-    targetEl.setAttribute('aria-describedby', tooltip.id); // Needs to be on trigger, not tooltip itself
+    targetEl.setAttribute('aria-describedby', tooltip.id); // Must be on trigger, not tooltip itself
 
     tooltip.textContent = text;
 
-    const targetElRect = targetEl.getBoundingClientRect(); // gives us position info for triggers
+    const targetElRect = targetEl.getBoundingClientRect();
     const isTall = targetElRect.height > 80;
-    const targetElCenterX = (targetElRect.left + targetElRect.right) / 2; // gives us dist from end to center for carrot
-    const y = isTall ? (targetElRect.top + 2 * (targetElRect.height / 3)) : targetElRect.bottom;
-    tooltip.style.position = 'fixed';
-    tooltip.style.top = `${y}px`;
-    tooltip.style.zIndex = '1000'; // TODO: Consider dropping to 100-199 range to adhere to z-index notes in app.css
-    document.body.appendChild(tooltip);
+    const centerX = (targetElRect.left + targetElRect.right) / 2; // gives us dist from end to center for caret
+    const y = isTall
+        ? (targetElRect.top + 2 * (targetElRect.height / 3))
+        : targetElRect.bottom;
 
-    const tooltipRect = tooltip.getBoundingClientRect();
-    tooltip.style.left = `${targetElCenterX - (tooltipRect.width / 2)}px`;
-    const arrowX = (tooltipRect.width / 2); // calc carrot position inside tooltip
-    tooltip.style.setProperty('--carrot-pos', `${arrowX}px`);
+    tooltip.style.zIndex = '1000'; // TODO: Consider dropping to 100-199 range to adhere to z-index notes in app.css
+
+    // Tooltips in dialogs need dialog parent to avoid stacking context issues
+    const parentDialogEl = targetEl.closest('dialog');
+    const isInDialog = parentDialogEl && parentDialogEl instanceof HTMLDialogElement;
+
+    if (isInDialog) {
+        parentDialogEl.appendChild(tooltip);
+    } else {
+        document.body.appendChild(tooltip);
+    }
+
+    const tooltipRect = tooltip.getBoundingClientRect(); // gives tooltips curr size and position 
+    const tooltipCenterOffset = (tooltipRect.width / 2); // calc caret position inside tooltip
+
+    if (isInDialog) {
+        const dialogRect = parentDialogEl.getBoundingClientRect();
+        
+        tooltip.style.position = 'absolute';
+        tooltip.style.top = `${y - dialogRect.top}px`;
+        tooltip.style.left = `${centerX - dialogRect.left - tooltipCenterOffset}px`;
+
+    } else {
+        tooltip.style.position = 'fixed';
+        tooltip.style.top = `${y}px`;
+        const tooltipRectNew = tooltip.getBoundingClientRect(); // Must measure after position: fixed since element width changes when removed from document flow
+        tooltip.style.left = `${centerX - (tooltipRectNew.width/2)}px`;
+    }
+    tooltip.style.setProperty('--caret-pos', '50%');
 }
 
-export function hideTooltip() {
+export function removeTooltip() {
     document.querySelector<HTMLElement>('#tooltip')?.remove();
 }
 
@@ -46,17 +74,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const tooltipTriggers = document.querySelectorAll<HTMLElement>('[data-tip]');
 
     // Add listener to each tooltip
-    tooltipTriggers.forEach(element => {
-        element.addEventListener('mouseenter', () => {
-            const tooltipText = element.getAttribute('data-tip');
+    tooltipTriggers.forEach(el => {
+        el.addEventListener('mouseenter', () => {
+            const tooltipText = el.getAttribute('data-tip');
             if (!tooltipText) {
-                console.warn('Tooltip element missing data-tip attribute: ', element);
+                console.warn('Tooltip element missing data-tip attribute: ', el);
                 return;
             }
-            showTooltip(element, tooltipText);
+            createTooltip(el, tooltipText);
         });
-        element.addEventListener('mouseleave', () => {
-            hideTooltip();
+        el.addEventListener('mouseleave', () => {
+            removeTooltip();
         });
     });
 });
