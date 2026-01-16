b8d71adce14a35ee3bd4962b4fc4ac6c65729df5
isaacrosdail <idrfritcher@gmail.com>
2026-01-16 01:23:35 -0600

    refactor: rename collections.py/types.py to avoid shadowing
    - collections.py -> collection_utils.py

--- app/shared/type_defs.py
+++ app/shared/type_defs.py
@@ -1,26 +1,28 @@
 from enum import Enum
 from functools import total_ordering
-from typing import Self, Union
+from typing import Self
 
 NotImplementedType = type(NotImplemented)
 
-@total_ordering  
+
+@total_ordering
 class OrderedEnum(Enum):
     """
     Base for sortable enums.
-    Current hacky usage: 
-    
+    Current hacky usage:
+
     class MY_ENUM(OrderedEnum):
         ...
 
     Below enum class, define MY_ENUM.sort_order as a list[str]
     """
-    
+
     def __lt__(self, other: Self) -> bool:
         if self.__class__ is not other.__class__:
             return NotImplemented
         # Access via __class__ attribute directly
-        order: list[str] | None = getattr(self.__class__, 'sort_order', None)
+        order: list[str] | None = getattr(self.__class__, "sort_order", None)
         if not order:
-            raise NotImplementedError(f"{self.__class__.__name__} must define sort_order")
+            msg = f"{self.__class__.__name__} must define sort_order"
+            raise NotImplementedError(msg)
         return order.index(self.name) < order.index(other.name)
