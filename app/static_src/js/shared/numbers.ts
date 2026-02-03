e668bc8f3d9c1b54093e5d477ff687d3965d6a00
isaacrosdail <idrfritcher@gmail.com>
2026-02-03 13:15:45 -0600

    comment(numbers.ts): note [min, max) for future self

--- app/static_src/js/shared/numbers.ts
+++ app/static_src/js/shared/numbers.ts
@@ -4,7 +4,11 @@ export function formatDecimal(value: number | string, precision = 2): string {
     return Number(value).toFixed(precision);
 }
 
+// TODO: Make this minIncl, maxExcl & ensure callsites are updated
+// half-open intervals follows indexing/iteration
+// this way the end is the length or count, not the last valid index
 export const randInt = (min: number, max: number) =>
+    // remove +1 here
     Math.floor(Math.random() * (max - min + 1)) + min;
 
 export const randFloat = (min: number, max: number) =>
